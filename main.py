bc248ed438fe3ddf460db4d72c039fa3d039e5dc
Pradnya Jeurkar <pradnya.ce@gmail.com>
2025-08-23 18:39:18 +0530

    Initial commit with main.py & .gitignore

---
+++ main.py
@@ -0,0 +1,23 @@
+todos = []
+while True:
+    user_action = input("Type add, show, edit or exit: ")
+    user_action = user_action.strip()
+
+    match user_action:
+        case 'add':
+            todo = input("Enter a todo: ")
+            todos.append(todo)
+        case "show" | 'display':
+            for item in todos:
+                print(item)
+        case "edit":
+            number = int(input("Enter the todo no. to edit: "))
+            number = number - 1   # because list indexing starts from 0, so when user writes 2, its actually 1.
+            new_todo = input("Enter new todo: ")
+            todos[number] = new_todo
+        case "exit":
+            break
+        # case _:
+        #     print("Invalid input! Try again.")
+
+print("Bye!")
