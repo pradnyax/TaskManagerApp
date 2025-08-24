ba1526398a47e5e3031c6af0ac55aaca3161739b
Pradnya Jeurkar <pradnya.ce@gmail.com>
2025-08-24 17:08:09 +0530

    read and write from text files added

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
-todos = []
 while True:
     user_action = input("Type add, show, edit, complete or exit: ")
     user_action = user_action.strip()
 
     match user_action:
         case 'add':
-            todo = input("Enter a todo: ")
+            todo = input("Enter a todo: ") + "\n"
+
+            file = open('todos.txt', 'r')   # r = read
+            todos = file.readlines()
+            file.close()    #NOTE: always close the file.
+
             todos.append(todo)
+
+            file = open('todos.txt', 'w')   # w = write
+            file.writelines(todos)
+            file.close()
         case "show" | 'display':
+            file = open('todos.txt', 'r')
+            todos = file.readlines()
+            file.close()
+
             for index, item in enumerate(todos):
                 row = f"{index + 1}-{item}"
                 print(row)
         case "edit":
             number = int(input("Enter the todo no. to edit: "))
-            number = number - 1   # because list indexing starts from 0, so when user writes 2, its actually 1. (has done this -1 or +1 on other lines of code too!)
+            number = number - 1   #because list indexing starts from 0, so when user writes 2, its actually 1. (has done this -1 or +1 on other lines of code too!)
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo
         case 'complete':
