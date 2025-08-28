a6b318fb57a2fbdf5ea38ebd268205ddc101c830
Pradnya Jeurkar <pradnya.ce@gmail.com>
2025-08-29 01:19:17 +0530

    custom function added

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+def get_todos():
+    with open('todos.txt', 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
 while True:
     # Get user input & strip space chars from it.
     user_action = input("Type add, show, edit, complete or exit: ")
@@ -7,8 +13,7 @@ while True:
     if user_action.startswith("add"):
         todo = user_action[4:]      #list slicing operation
 
-        with open('todos.txt', 'r') as file:
-            todos = file.readlines()
+        todos = get_todos()
 
         todos.append(todo + '\n')
 
@@ -18,8 +23,7 @@ while True:
     # elif "show" in user_action:
     elif user_action.startswith("show"):
 
-        with open('todos.txt', 'r') as file:
-            todos = file.readlines()
+        todos = get_todos()
 
         for index, item in enumerate(todos):
             item = item.strip('\n')
@@ -33,8 +37,7 @@ while True:
             print(number)
             number = number - 1   #because list indexing starts from 0, so when user writes 2, its actually 1. (has done this -1 or +1 on other lines of code too!)
 
-            with open('todos.txt', 'r') as file:
-                todos = file.readlines()
+            todos = get_todos()
 
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo + '\n'
@@ -50,8 +53,7 @@ while True:
         try:
             number = int(user_action[9:])
 
-            with open('todos.txt', 'r') as file:
-                todos = file.readlines()
+            todos = get_todos()
 
             index = number - 1
             todo_to_remove = todos[index].strip('\n')
