9195f4194bd6d064ea1bae684dbae42971bd137c
Pradnya Jeurkar <pradnya.ce@gmail.com>
2025-08-30 00:13:16 +0530

    write function with parameters added

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-def get_todos():
-    with open('todos.txt', 'r') as file_local:
+def get_todos(filepath):
+    with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
+
+def write_todos(filepath, todos_arg):
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
 
 
 while True:
@@ -13,17 +17,16 @@ while True:
     if user_action.startswith("add"):
         todo = user_action[4:]      #list slicing operation
 
-        todos = get_todos()
+        todos = get_todos("todos.txt")
 
         todos.append(todo + '\n')
 
-        with open('todos.txt', 'w') as file:
-            file.writelines(todos)
+        write_todos("todos.txt", todos)
 
     # elif "show" in user_action:
     elif user_action.startswith("show"):
 
-        todos = get_todos()
+        todos = get_todos("todos.txt")
 
         for index, item in enumerate(todos):
             item = item.strip('\n')
@@ -37,13 +40,12 @@ while True:
             print(number)
             number = number - 1   #because list indexing starts from 0, so when user writes 2, its actually 1. (has done this -1 or +1 on other lines of code too!)
 
-            todos = get_todos()
+            todos = get_todos("todos.txt")
 
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo + '\n'
 
-            with open('todos.txt', 'w') as file:
-                file.writelines(todos)
+            write_todos("todos.txt", todos)
         except ValueError:
             print("Invalid command.")
             continue        #continue jumps back to the beginning.
@@ -53,14 +55,13 @@ while True:
         try:
             number = int(user_action[9:])
 
-            todos = get_todos()
+            todos = get_todos("todos.txt")
 
             index = number - 1
             todo_to_remove = todos[index].strip('\n')
             todos.pop(index)
 
-            with open('todos.txt', 'w') as file:
-                file.writelines(todos)
+            write_todos("todos.txt", todos)
 
             message = f"Todo {todo_to_remove} was removed from the list."
             print(message)
