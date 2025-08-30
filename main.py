a822618eaaa862cf9e01b4a4fdf9cc0750096b79
Pradnya Jeurkar <pradnya.ce@gmail.com>
2025-08-30 23:34:27 +0530

    functions module added

--- main.py
+++ main.py
@@ -1,17 +1,5 @@
-def get_todos(filepath="todos.txt"):
-    """Read a text file and return the list of to-do items."""
-    with open(filepath, 'r') as file_local:
-        todos_local = file_local.readlines()
-    return todos_local
-
-
-# def write_todos(filepath="todos.txt", todos_arg): ---> wrong!
-# non-default parameters should come before default parameters!
-def write_todos(todos_arg, filepath="todos.txt"):
-    """Write a list of to-do items in a text file."""
-    with open(filepath, 'w') as file:
-        file.writelines(todos_arg)
-
+# from functions import get_todos, write_todos
+import functions
 
 while True:
     # Get user input & strip space chars from it.
@@ -22,17 +10,17 @@ while True:
     if user_action.startswith("add"):
         todo = user_action[4:]      #list slicing operation
 
-        todos = get_todos()
+        todos = functions.get_todos()
 
         todos.append(todo + '\n')
 
         # write_todos(todos, "todos.txt") --> don't need the filepath here coz it's a default parameter!
-        write_todos(todos)
+        functions.write_todos(todos)
 
     # elif "show" in user_action:
     elif user_action.startswith("show"):
 
-        todos = get_todos()
+        todos = functions.get_todos()
 
         for index, item in enumerate(todos):
             item = item.strip('\n')
@@ -46,12 +34,12 @@ while True:
             print(number)
             number = number - 1   #because list indexing starts from 0, so when user writes 2, its actually 1. (has done this -1 or +1 on other lines of code too!)
 
-            todos = get_todos()
+            todos = functions.get_todos()
 
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo + '\n'
 
-            write_todos(todos)
+            functions.write_todos(todos)
         except ValueError:
             print("Invalid command.")
             continue        #continue jumps back to the beginning.
@@ -61,13 +49,13 @@ while True:
         try:
             number = int(user_action[9:])
 
-            todos = get_todos()
+            todos = functions.get_todos()
 
             index = number - 1
             todo_to_remove = todos[index].strip('\n')
             todos.pop(index)
 
-            write_todos(todos)
+            functions.write_todos(todos)
 
             message = f"Todo {todo_to_remove} was removed from the list."
             print(message)
