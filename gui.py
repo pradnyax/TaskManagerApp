bd589cac083ae3a723e846f467a3c0f76b67b641
Pradnya Jeurkar <pradnya.ce@gmail.com>
2025-09-06 20:08:02 +0530

    Add clock feature and change theme

--- gui.py
+++ gui.py
@@ -1,27 +1,32 @@
+import time
+
 import functions
 import FreeSimpleGUI as sg
 
+sg.theme("DarkPurple4")
+
+clock = sg.Text('', key='clock')
 label = sg.Text("Type in a to-do:")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
-add_button = sg.Button("Add")
+add_button = sg.Button("Add", size=10)
 list_box = sg.Listbox(values=functions.get_todos(), key='todos',
                       enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
 complete_button = sg.Button("Complete")
-exit_button = sg.Button("Exit")
+exit_button = sg.Button("Exit", size=10)
 
 window = sg.Window("My To-Do App",
-                   layout=[[label],
+                   layout=[[clock],
+                           [label],
                            [input_box, add_button],
                            [list_box, edit_button, complete_button],
                            [exit_button]],
                    font=('Helvetica', 20))
 
 while True:
-    event, values = window.read()
-    print(1, event)
-    print(2, values)
-    print(3, values['todos'])
+    event, values = window.read(timeout=200)
+    window["clock"].update(value=time.strftime("%d %B, %Y %I:%M:%S"))
+
     match event:
         case "Add":
             todos = functions.get_todos()
