f057404e851ceff23bca0dedf0292c04c89e33a4
Pradnya Jeurkar <pradnya.ce@gmail.com>
2026-08-15 00:17:19 +0530

    feat: add Listbox widget and Edit functionality
    - Display saved todos in a Listbox widget with event listening
    - Populate input box with selected todo on item click
    - Enable editing and updating todos in both file storage and UI listbox

--- gui.py
+++ gui.py
@@ -5,14 +5,20 @@ import FreeSimpleGUI as sg
 label = sg.Text("Type in a to-do:")
 input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_Button = sg.Button("Add")
+list_box = sg.Listbox(values=functions.get_todos(), key='list_todos',
+                      enable_events=True, size=[44, 10])
+edit_button = sg.Button("Edit")
 
 window = sg.Window("P's To-Do App",
-                   layout=[[label], [input_box, add_Button]],
+                   layout=[[label],
+                           [input_box, add_Button],
+                           [list_box, edit_button]],
                    font=('Helvetica', 20))
 while True:
     event, values = window.read()
-    print(event)
-    print(values)
+    print(1, event)
+    print(2, values)
+    print(3, values['list_todos'])
 
     match event:
         case "Add":
@@ -20,6 +26,18 @@ while True:
             new_todo = values['todo'] + "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
+            window['list_todos'].update(values=todos)
+        case "Edit":
+            todo_to_edit = values['list_todos'][0]
+            new_todo = values['todo']
+
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            window['list_todos'].update(values=todos)
+        case "list_todos":
+            window['todo'].update(value=values['list_todos'][0])
         case sg.WIN_CLOSED:
             break
 
